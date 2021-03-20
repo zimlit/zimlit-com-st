d47c00ad524829e0449e490665d86cda115d1261
zimlit <zimlit2010@gmail.com>
2021-03-20 14:33:43 -0400

    users

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,14 @@
 Rails.application.routes.draw do
+  resources :users, only: [:new, :create]
+  get 'login', to: 'sessions#new'
+  post 'login', to: 'sessions#create'
+  get 'welcome', to: 'sessions#welcome'
   get 'conlanging', to: "conlanging#index"
   get 'conworlding', to: "conworlding#index"
   get 'cubing', to: "cubing#index"
   get 'coding', to: "coding#index"
   get 'home', to: "home#index"
+  delete 'logout', to: "sessions#destroy"
   root 'home#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
