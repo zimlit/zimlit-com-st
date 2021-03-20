88a2cb9b50fec25156187e4b5eee21f968d09487
zimlit <zimlit2010@gmail.com>
2021-03-19 21:41:58 -0400

    lock

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  get 'coding/index'
+  get 'coding', to: "coding#index"
   get 'home/index'
   root 'home#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
